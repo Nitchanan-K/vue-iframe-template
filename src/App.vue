471e935936075c98586081d1b7dc08152d5e645a
nitchanan.k <nitchanan_katinasatian@hotmail.com>
2023-12-25 11:40:37 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+</script>
+
+<template >
+
+  <div class="container" >
+    <iframe class="responsive-iframe"
+      src="http://localhost:5174/add-product-"
+      >
+    </iframe>
+  </div>
+
+</template>
+
+<style scoped>
+
+
+.container {
+  width: 1080px;
+  height: 1300px;
+}
+.responsive-iframe {
+  position: relative;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  width: 100%;
+  height: 100%;
+}
+</style>
